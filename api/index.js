95bbf6c8e63a946088aced9f666471352130f582
madhuvikas <madhurijagtap0123@gmail.com>
2022-01-10 13:40:35 +0530

    api file changes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const express =require('express');
 const router = express.Router();
 const app = express()
-    router.use((req,res,next)=>{
-        Object.setPrototypeOf(req, app.request)
-        Object.setPrototypeOf(res,app.response)
-        req.res = res
-        res.req = reqnext()
-    })
+router.use((req, res, next) => {
+  Object.setPrototypeOf(req, app.request)
+  Object.setPrototypeOf(res, app.response)
+  req.res = res
+  res.req = req
+  next()
+})
 
 router.post('/track-data',(req,res)=>{
     console.log("Stored data!", req.body.data);
